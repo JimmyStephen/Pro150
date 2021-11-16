41d46a7ed21dfd6eb67d53fa4a4db156a2a0514e
Kaleb Washington <kdwashington302@gmail.com>
2021-11-15 19:37:59 -0700

    refactored

--- src/main/java/com/Pro150/Casino/RestController.java
+++ src/main/java/com/Pro150/Casino/RestController.java
@@ -2,9 +2,9 @@ package com.Pro150.Casino;
 
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@org.springframework.web.bind.annotation.RestController
 @RequestMapping("/casino")
-public class Controller {
+public class RestController {
 
 
     @RequestMapping("hello")
