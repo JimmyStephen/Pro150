f5036b8a1feaff6d5c12f6ba7e2dd0e875712dc3
JimmyStephen <jstephen@student.neumont.edu>
2021-11-15 19:35:49 -0700

    Added the Deck Class in Shared that contains an enum for decks of cards and methods to use that deck

--- src/main/java/com/Pro150/Casino/Shared/Deck.java
+++ src/main/java/com/Pro150/Casino/Shared/Deck.java
@@ -11,13 +11,17 @@ public class Deck {
         newDeck();
     }
 
+    //will return how many cards are in the deck
     public int remainingCards(){
         return deck.size() - 1;
     }
 
+    //will create a new deck of 52 cards
     public  void newDeck(){
         deck.addAll(Arrays.asList(Card.values()).subList(0, 52));
     }
+
+    //will shuffle the deck using all the cards currently in the deck
     public void shuffleDeck(){
         ArrayList<Card> retVal = new ArrayList<>();
         int i = 0;
@@ -29,12 +33,16 @@ public class Deck {
         }
         deck = retVal;
     }
+
+    //Will return the value of a card and remove it from the current deck
     public Card drawCard(){
         Card retVal = deck.get(0);
         deck.remove(0);
         return retVal;
     }
 
+    //Contains all 52 cards (No Jokers) and 4 additional cards for Aces called D_ACE_OF_X used for values of 1 instead of 11
+    //These will not be used with the deck unless specifically called
     public enum Card{
         TWO_OF_HEARTS(2),
         THREE_OF_HEARTS(3),
