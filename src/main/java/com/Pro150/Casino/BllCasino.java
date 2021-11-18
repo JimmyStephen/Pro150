09f639c663905a4e1e1c85f5cdafc70695b70a91
Kaleb Washington <kdwashington302@gmail.com>
2021-11-17 19:11:38 -0700

    connected database

--- src/main/java/com/Pro150/Casino/BllCasino.java
+++ src/main/java/com/Pro150/Casino/BllCasino.java
@@ -1,4 +1,22 @@
 package com.Pro150.Casino;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static com.mongodb.client.model.Filters.*;
+
 public class BllCasino {
-}
+   //
+   private final MongoCollection<Document> userCollection;
+
+    public BllCasino() {
+        var client = MongoClients.create("mongodb+srv://group:pro150@userinformation.xrasy.mongodb.net/UserInformation?retryWrites=true&w=majority");
+        var database = client.getDatabase("UserInformation");
+        userCollection = database.getCollection("Users");
+
+        }
+    }
