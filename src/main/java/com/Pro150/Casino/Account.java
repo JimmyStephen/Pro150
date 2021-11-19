626b1701ee9142d29c4f858d443a9d23efcb566e
Kaleb Washington <kdwashington302@gmail.com>
2021-11-18 18:47:31 -0700

    accounts and things

--- src/main/java/com/Pro150/Casino/Account.java
+++ src/main/java/com/Pro150/Casino/Account.java
@@ -3,9 +3,39 @@ package com.Pro150.Casino;
 import java.util.ArrayList;
 
 public class Account {
+
     String username;
     String password;
     int currency;
+
     ArrayList<Ticket> tickets = new ArrayList<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(int currency) {
+        this.currency = currency;
+    }
+
+    public ArrayList<Ticket> getTickets() {
+        return tickets;
+    }
 
 }
--- src/main/java/com/Pro150/Casino/Ticket.java
+++ src/main/java/com/Pro150/Casino/Ticket.java
@@ -1,5 +1,21 @@
 package com.Pro150.Casino;
 
-public class Ticket {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Ticket {
+    int[] numbers =new int[7];
+    private static int getRandomInt(int min, int max){
+        Random rng = new Random();
+        int randNum = rng.nextInt(max - min + 1)+ min;
+        return randNum;
+    }
+
+public void fillticket(){
+    for(int i=0; i<numbers.length; i++){
+
+        numbers[i] = getRandomInt(1,99);
+    }
+    }
 }
