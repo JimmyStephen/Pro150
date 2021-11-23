5b4a9c6bd082755697b6a15438563d274faf2481
The64GigKid <47188661+The64GigKid@users.noreply.github.com>
2021-11-22 19:15:36 -0700

    Update Controller.java

--- src/main/java/com/Pro150/Casino/Slots/Controller.java
+++ src/main/java/com/Pro150/Casino/Slots/Controller.java
@@ -1,4 +1,66 @@
 package com.Pro150.Casino.Slots;
 
 public class Controller {
+  private static int[] rewards = {1000,2000,5000,8000,10000,20000,50000};
+    private static int slotPos = 0;
+    private enum Icons{
+        grape,
+        strawberry,
+        Seven,
+        Bar,
+        Lemon,
+        Chest,
+        Pie
+    };
+    private static Icons[] icons = new Icons[3];
+
+    public static void main(String[] args)  throws IOException{
+        Spin();
+    }
+
+    public static void Spin(){
+        Random rand = new Random();
+        int slot = rand.nextInt(7);
+        icons[slotPos] = GetIcon(slot);
+        slotPos++;
+        if(slotPos == 3){
+            winLose(slot);
+        }
+    }
+    public static int winLose(int slot){
+        if(icons[1] == icons[2] && icons[1] == icons[3]){
+            slotPos = 0;
+            return rewards[slot];
+        }
+        else{
+            slotPos = 0;
+            return 0;
+        }
+    }
+    public static Icons GetIcon(int i){
+        if(i == 1){
+            return Icons.grape;
+        }
+        else if(i == 2){
+            return Icons.Lemon;
+        }
+        else if(i == 3){
+            return Icons.strawberry;
+        }
+        else if(i == 4){
+            return Icons.Pie;
+        }
+        else if(i == 5){
+            return Icons.Chest;
+        }
+        else if(i == 6){
+            return Icons.Bar;
+        }
+        else if(i == 7){
+            return Icons.Seven;
+        }
+        else{
+            return null;
+        }
+    }
 }
